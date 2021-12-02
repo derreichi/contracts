18d4ebbff226fbe5f21d024f54e3af66fe2bca4f
reichst <steffen.reichert@ext.hlag.com>
2021-12-02 14:26:25 +0100

    [PHX-123] supress kafka beans starting in ide

--- src/main/java/de/reichi/database/KafkaProducerConfig.java
+++ src/main/java/de/reichi/database/KafkaProducerConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -13,6 +14,7 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
 
+@Profile("!ide")
 @Configuration
 public class KafkaProducerConfig {
 
