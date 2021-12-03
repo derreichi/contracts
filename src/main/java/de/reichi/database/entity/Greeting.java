84dd332c775059341477c72b6c5a7d3fe71f1d18
reichst <steffen.reichert@ext.hlag.com>
2021-12-03 12:18:34 +0100

    [PHX-124] added security

--- src/main/java/de/reichi/database/entity/Greeting.java
+++ src/main/java/de/reichi/database/entity/Greeting.java
@@ -1,7 +1,21 @@
 package de.reichi.database.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Greeting {
-  public final String message;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String message;
 
   public Greeting(String name) {
     this.message = name;
