994d8ce85250398872d8d9e30c835882e7319cdf
reichst <steffen.reichert@ext.hlag.com>
2021-12-14 16:40:42 +0100

    [PHX-126] dont use user as a table name without quotation maarks

--- src/main/java/de/reichi/database/controller/RegistrationController.java
+++ src/main/java/de/reichi/database/controller/RegistrationController.java
@@ -1,11 +1,5 @@
 package de.reichi.database.controller;
 
-import de.reichi.database.entity.Greeting;
-import de.reichi.database.entity.User;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/de/reichi/database/entity/User.java
+++ src/main/java/de/reichi/database/entity/User.java
@@ -4,17 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,7 +24,7 @@ public class User {
     private String password;
 
     @Email(regexp = ".+@.+\\..+", message = "An email has to look like an email!!!")
-    //@Column (unique = true) // 1 Email = 1 User
+    @Column (unique = true) // 1 Email = 1 User
     private String email;
 
     private String role;
